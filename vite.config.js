b2774835b0f5487a40459fe15a243ff4b9b397fa
yasser8111 <yasserxd653@gmail.com>
2026-01-02 14:29:37 +0300

    fix: explicit output directory for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), tailwindcss()],
 });
